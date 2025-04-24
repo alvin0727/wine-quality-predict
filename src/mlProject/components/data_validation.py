22ab8ac94b8141c9a5e1c644d9000f8cf4bf2d85
Alvin7-boys <alvinboys270702@gmail.com>
2025-04-25 01:43:35 +0700

    feat: add data validation

---
+++ src/mlProject/components/data_validation.py
@@ -0,0 +1,34 @@
+import os
+from mlProject import logger 
+from mlProject.entity.config_entity import DataValidationConfig
+import pandas as pd
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+        
+    def validate_all_columns(self) -> bool:
+        try:
+            # Read data
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+            all_schema = self.config.all_schema.keys()
+
+            validation_status = True
+
+            # Validation the columns
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    break  
+
+            os.makedirs(os.path.dirname(self.config.STATUS_FILE), exist_ok=True)
+
+            # Write the validation
+            with open(self.config.STATUS_FILE, "w") as f:
+                f.write(f"Validation Status: {validation_status}")
+
+            return validation_status
+
+        except Exception as e:
+            raise e
